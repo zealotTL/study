9673546a0a13e49678c730837e4e7db9d3477767
zealotTL <t953170224@163.com>
2019-11-03 22:16:11 +0800

    activemq 处理

--- datasource/src/main/java/group/zealot/study/datasource/Run.java
+++ datasource/src/main/java/group/zealot/study/datasource/Run.java
@@ -1,14 +1,27 @@
 package group.zealot.study.datasource;
 
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.ActiveMQSession;
+import org.apache.activemq.RedeliveryPolicy;
+import org.messaginghub.pooled.jms.JmsPoolConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 
 @SpringBootApplication
 public class Run {
@@ -22,6 +35,7 @@ public class Run {
         Run run = context.getBean(Run.class);
         run.studyJdbc();
         run.studyRedis();
+        run.studyActimvemq();
     }
 
     @Autowired
@@ -39,4 +53,37 @@ public class Run {
         String name = redisTemplate.opsForValue().get("name");
         logger.info("name: " + name);
     }
+
+
+    @Bean
+    public DefaultJmsListenerContainerFactory defaultJmsListenerContainerFactory(JmsPoolConnectionFactory jmsPoolConnectionFactory) {
+        ActiveMQConnectionFactory factory = (ActiveMQConnectionFactory) jmsPoolConnectionFactory.getConnectionFactory();
+        RedeliveryPolicy redeliveryPolicy = factory.getRedeliveryPolicy();
+        redeliveryPolicy.setUseExponentialBackOff(false);//重发时间间隔递增
+        redeliveryPolicy.setBackOffMultiplier(2.0);//重发时间间隔倍数增长，需 UseExponentialBackOff 为true
+        redeliveryPolicy.setMaximumRedeliveries(5);//最大重发次数
+        redeliveryPolicy.setMaximumRedeliveryDelay(3000);//最大重发间隔时间
+        redeliveryPolicy.setInitialRedeliveryDelay(1000);//初始重发时间间隔
+
+        DefaultJmsListenerContainerFactory defaultJmsListenerContainerFactory = new DefaultJmsListenerContainerFactory();
+        defaultJmsListenerContainerFactory.setConnectionFactory(jmsPoolConnectionFactory);
+        defaultJmsListenerContainerFactory.setSessionAcknowledgeMode(ActiveMQSession.INDIVIDUAL_ACKNOWLEDGE);//客户端调用acknowledge方法手动签收  activemq专用4
+        defaultJmsListenerContainerFactory.setPubSubDomain(false);
+        return defaultJmsListenerContainerFactory;
+    }
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    private void studyActimvemq() {
+        jmsTemplate.convertAndSend("test", "this test message");
+    }
+
+
+    @JmsListener(destination = "test",containerFactory = "defaultJmsListenerContainerFactory")
+    protected void test(Session session, Message message) throws JMSException {
+        TextMessage textMessage = (TextMessage) message;
+        logger.info("jms: " + textMessage.getText());
+    }
+
 }
