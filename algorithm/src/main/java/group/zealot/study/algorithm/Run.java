70e2138c815fd2d02ac01d720a4dc60e15b79234
zealotTL <t953170224@163.com>
2019-11-07 22:03:14 +0800

    ==

--- algorithm/src/main/java/group/zealot/study/algorithm/Run.java
+++ algorithm/src/main/java/group/zealot/study/algorithm/Run.java
@@ -1,6 +1,6 @@
 package group.zealot.study.algorithm;
 
-import group.zealot.study.algorithm.sort.MaoPaoSort;
+import group.zealot.study.algorithm.sort.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
