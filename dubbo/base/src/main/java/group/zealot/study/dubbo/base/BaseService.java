def04ba6464d59e5910924fffcb065906f9cfdd0
zealot <t953170224@163.com>
2020-10-31 09:49:01 +0800

    删除dubbo模块

--- dubbo/base/src/main/java/group/zealot/study/dubbo/base/BaseService.java
+++
@@ -1,8 +0,0 @@
-package group.zealot.study.dubbo.base;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class BaseService {
-    protected Logger logger = LoggerFactory.getLogger(getClass());
-}
